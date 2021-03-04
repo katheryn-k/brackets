d6ce9d92e254d42a2298fd99ebed2619de31e16f
katheryn-k <kirichenko.ekaterynaa@yandex.ru>
2021-03-04 13:26:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map(item => item.join(''));
+    let i = 0;
+    while (i < brackets.length) {
+        if (str.includes(brackets[i])) {
+            str = str.replace(brackets[i], '');
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    let result = str ? false : true;
+    return result;
 }
